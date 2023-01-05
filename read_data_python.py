411c02b161c360392e65036db3a495770756fc2e
Tim Player <timrplayer@gmail.com>
2023-01-06 04:00:17 +0800

    Add args

--- read_data_python.py
+++ read_data_python.py
@@ -1,39 +1,37 @@
-# test.py
+# read_data_python.py
 # Tim Player, January 1 2023
 # Test script that connects to a CR1000 on COM3 on a Windows computer and 
 # downloads the last hour of data from the 'Radiation' table.
 # The script prints the data and writes it to a file.
-import serial
-from pycampbellcr1000 import CR1000 # pip install pycambellcr1000
+
 import pandas as pd # pip install pandas
 import datetime
+import argparse
+from cr1000utils import connect_to_device
 
-# Open a connection to the CR1000 using PyCambellCR1000.
-# Use PySerial instead of PyLink to make the connection more reliable on Windows
-# as described here:
-# https://github.com/LionelDarras/PyCampbellCR1000/issues/21#issuecomment-1117096281
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", type=str, default="/dev/ttyUSB0", help="Port to connect to like /dev/ttyUSB0")
+parser.add_argument("--hours", type=int, default="0", help="Integer number of hours into the past of data to retrieve. 0 for all data.")
+parser.add_argument("--output", type=str, default="data.csv", help="Path of output CSV file.")
 
-# use port=None to create a serial port object without opening the underlying port
-ser = serial.Serial(port=None, 
-    baudrate=115200,
-    timeout=2,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=1)
+if __name__ == "__main__":
 
-ser.port = "COM3"
-cr1000 = CR1000(ser)
+    args = parser.parse_args()
 
+    cr1000 = connect_to_device(args.port)
 
-# Print out the tables stored by the CR1000
-print(cr1000.list_tables())
+    # Print out the tables stored by the CR1000
+    print(f"Tables stored in device: \n{cr1000.list_tables()}")
 
-# Retrieve and print the last hour of data
-end = cr1000.gettime()
-start = end - datetime.timedelta(hours=1)
+    start, end = None, None
+    
+    if args.hours != 0:
+        # Retrieve and print the last n hours of data if requested
+        end = cr1000.gettime()
+        start = end - datetime.timedelta(hours=args.hours)
 
-lwdata = cr1000.get_data('Radiation', start, end) # Read dictionary from CR1000
-lwdf = pd.DataFrame(lwdata) # convert to Pandas DataFrame for prettier formatting
-
-print(lwdf) # print the table
-lwdf.to_csv('last_hour.csv') # write to CSV file
+    print(f"Getting data from {start} to {end}.")
+    lwdata = cr1000.get_data('Radiation', start_date=start, stop_date=end) # Read dictionary from CR1000
+    lwdf = pd.DataFrame(lwdata) # convert to Pandas DataFrame for prettier formatting
+    print(lwdf) # print the table
+    lwdf.to_csv(args.output) # write to CSV file
