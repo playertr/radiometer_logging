48f9a18f9be0bddda1c23b64f25f9c2de26f2ac2
Tim Player <timrplayer@gmail.com>
2023-01-06 04:00:07 +0800

    Add utility module

---
+++ cr1000utils.py
@@ -0,0 +1,21 @@
+
+from pycampbellcr1000 import CR1000 # pip install git+https://github.com/playertr/PyCampbellCR1000.git
+import serial
+
+def connect_to_device(port : str = "/dev/ttyUSB0") -> CR1000:
+    # Open a connection to the CR1000 using PyCambellCR1000.
+    # Use PySerial instead of PyLink to make the connection more reliable on Windows
+    # as described here:
+    # https://github.com/LionelDarras/PyCampbellCR1000/issues/21#issuecomment-1117096281
+
+    # use port=None to create a serial port object without opening the underlying port
+    ser = serial.Serial(port=None, 
+        baudrate=115200,
+        timeout=2,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=1)
+
+    ser.port = port
+    cr1000 = CR1000(ser)
+    return cr1000
--- set_time.py
+++ set_time.py
@@ -1,9 +1,11 @@
+# set_time.py
+# Tim Player, January 5 2023
+# Script that connects to a CR1000 and sets the time from an
+# NTP server or from the device's present time.
+
 import ntplib
 import datetime as dt
-
-import serial
-from pycampbellcr1000 import CR1000 # pip install git+https://github.com/playertr/PyCampbellCR1000.git
-import pandas as pd # pip install pandas
+from cr1000utils import connect_to_device
 
 """Query the datetime from an NTP server"""
 def get_time_ntp() -> dt.datetime:
@@ -16,24 +18,6 @@ def get_time_ntp() -> dt.datetime:
 """Get the current time from this computer"""
 def get_time_local() -> dt.datetime:
     return dt.datetime.now()
-
-def connect_to_device(port : str = "/dev/ttyUSB0") -> CR1000:
-    # Open a connection to the CR1000 using PyCambellCR1000.
-    # Use PySerial instead of PyLink to make the connection more reliable on Windows
-    # as described here:
-    # https://github.com/LionelDarras/PyCampbellCR1000/issues/21#issuecomment-1117096281
-
-    # use port=None to create a serial port object without opening the underlying port
-    ser = serial.Serial(port=None, 
-        baudrate=115200,
-        timeout=2,
-        bytesize=serial.EIGHTBITS,
-        parity=serial.PARITY_NONE,
-        stopbits=1)
-
-    ser.port = port
-    cr1000 = CR1000(ser)
-    return cr1000
 
 def as_utc_str(time: dt.datetime) -> str:
     return time.replace(tzinfo=dt.timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
